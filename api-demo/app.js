ba2fb0f56149f1724092ffd982c725e6429118b2
GiaHaoTran <19521481@gm.uit.edu.vn>
2021-11-18 17:21:41 +0700

    Update app.js

--- api-demo/app.js
+++ api-demo/app.js
@@ -5,7 +5,7 @@ const mongoClient = require('mongoose')
 
 // setup connect mongodb by mongoose
 //const URL = 'mongodb+srv://doadmin:Z6I25JV13Y40Udn9@db-mongodb-sgp1-13649-ec8f6987.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-sgp1-13649&tls=true&tlsCAFile=ca-certificate.crt'
-mongoClient.connect(process.env.DATABASE_URL, {
+mongoClient.connect(process.env.DATABASE_URL2, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
