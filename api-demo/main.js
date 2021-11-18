d5d3d4870cc673f4c1d6bd5f78dffdbce864009c
GiaHaoTran <19521481@gm.uit.edu.vn>
2021-11-18 10:18:54 +0700

    up

--- api-demo/main.js
+++ api-demo/main.js
@@ -1,63 +1,30 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const logger = require('morgan')
-const mongoClient = require('mongoose')
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-// setup connect mongodb by mongoose
-//const URL = 'mongodb+srv://doadmin:17F3aH68U0t9gLO4@db-mongodb-sgp1-13649-ec8f6987.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-sgp1-13649&tls=true&tlsCAFile=ca-certificate.crt'
-mongoClient.connect('', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log('✅ Connected database from mongodb.'))
-    .catch((error) => console.error(`❌ Connect database is failed with error which is ${error}`))
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
 
-const app = express()
+// Add custom routes before JSON Server router
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
 
-const userRoute = require('./routes/user')
-const categoryRoute = require('./routes/category')
-const productRoute = require('./routes/product')
-const cartRoute = require('./routes/cart')
-//const cartitemRoute = require('./routes/cartitem')
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
 
-// Middlewares
-app.use(logger('dev'))
-app.use(bodyParser.json())
-
-// Routes
-app.use('/users', userRoute)
-app.use('/categories', categoryRoute)
-app.use('/products', productRoute)
-app.use('/cart', cartRoute)
-//app.use('/cartitem', cartitemRoute)
-
-// Routes
-app.get('/', (req, res, next) => {
-    return res.status(200).json({
-        message: 'Server is OK!'
-    })
-})
-
-// Catch 404 Errors and forward them to error handler
-app.use((req, res, next) => {
-    const err = new Error('Not Found')
-    err.status = 404
-    next(err)
-})
-
-// Error handler function
-app.use((err, req, res, next) => {
-    const error = app.get('env') === 'development' ? err : {}
-    const status = err.status || 500
-
-    // response to client
-    return res.status(status).json({
-        error: {
-            message: error.message
-        }
-    })
-})
-
-// Start the server
-const port = app.get('port') || 3000
-app.listen(port, () => console.log(`Server is listening on port ${port}`))
+// Use default router
+const PORT = process.env.PORT || 3000;
+server.use(router);
+server.listen(PORT, () => {
+  console.log("JSON Server is running");
+});
